6e95f147eb683acc615db577ad5c1cb618270b52
yobe8 <sanyobe@gmail.com>
2021-05-06 18:08:30 +0900

    Clean codes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,11 @@ const Home = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>생명공감</h1>
       <div className={styles.homeImageContainer}>
-        <img className={styles.homeImage} src={homeImage} alt="dogs watching you over the wall" />
+        <img
+          className={styles.homeImage}
+          src={homeImage}
+          alt="dogs watching you over the wall"
+        />
       </div>
       <button className={styles.signInButton}>
         <img className={styles.googleIcon} src={googleIcon} width={50} />
