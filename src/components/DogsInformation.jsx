32f8300bb220513ff628bc496b5d4d6c05a65077
yobe8 <sanyobe@gmail.com>
2021-05-08 09:56:07 +0900

    WIP DogsInformation

---
+++ src/components/DogsInformation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Container from "./shared/Container";
+import ModalHeader from "./shared/ModalHeader";
+import InputButton from "./shared/InputButton";
+import Input from "./shared/Input";
+
+const DogsInformation = () => {
+  return (
+    <Container>
+      <ModalHeader text={"강아지들"}>
+      
+      </ModalHeader>
+    </Container>
+  );
+};
+
+export default DogsInformation;
